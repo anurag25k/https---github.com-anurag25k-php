a5f411b2a73d83e3f8d86301591acd560732600b
Anurag Kumar <103134980+anurag25k@users.noreply.github.com>
2024-02-05 22:54:05 +0530

    php

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- <form action="index.php" method="post">
+        <label for="">username</label>
+        <input type="text" name="username" id="">
+        <label for="">password</label>
+        <input type="text" name="password" id="">
+        <input type="submit" value="Log in">
+    </form> -->
+    <!-- <form action="index.php" method="post">
+        <label for="">Quantity:</label><br>
+        <input type="" name="quantity">
+        <input type="submit" value="total">
+    </form> -->
+    <!-- <form action="index.php" method="post">
+        <label for="">x:</label>
+        <input type="text" name="x">
+        
+        <br>
+        <label for="">y:</label>
+        <input type="text" name="y">
+       
+        <br>
+        <label for="">z:</label>
+        <input type="text" name="z">
+        <input type="submit" value="total"> -->
+
+    </form>
+</body>
+
+</html>
+<?php
+//username password
+//    echo "{$_POST["username"]} <br>";
+//    echo "{$_POST["password"]} <br>";
+
+// Pizzza shop
+//$item="pizza";
+// $price=5.99;
+// $quantity=$_POST["quantity"];
+// $total=null;
+
+// $total=$quantity*$price;
+
+// echo"You have ordered {$quantity} x {$item}/s";
+// echo"Your total is: \${$total}";
+$x = $_POST["x"];
+$y = $_POST["y"];
+$z = $_POST["z"];
+$total = null;
+
+// $total = abs($x);
+// $total = round($x);
+// $total = floor($x);
+// $total = ceil($x);
+// $total = pow($x,$y);
+// $total = sqrt($y);
+// $total=max($x,$y,$z);
+// $total=min($x,$y,$z);
+//$total=pi();
+// $total=rand(1,100);
+
+// echo "$total ";
+
+
+?>
